c9210478350d269ca7946e90e9e4dbf08fece1b1
Raj Gohil <raj.famous007@gmail.com>
2023-02-27 00:59:44 +0530

    Created the single class to Take the user input with the min max array length validation and Sort the provided array using the bubble sort technique.

--- src/com/raj/BinarySearchProgram.java
+++ src/com/raj/BinarySearchProgram.java
@@ -2,9 +2,11 @@ package com.raj;
 
 import java.util.Scanner;
 
-public class BinarySearchProgram extends BubbleSort{
+public class BinarySearchProgram extends HelperClass {
     public static void main(String[] args) {
 
+        // Take the input from the user with provided validation.
+        int[] arr = takeInputAndReturnArray(1, 11);
 
         // Sort the user provided array.
         bubbleSorting(arr);
--- src/com/raj/HelperClass.java
+++ src/com/raj/HelperClass.java
@@ -1,9 +1,11 @@
 package com.raj;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class BubbleSort {
+public class HelperClass {
 
+    // Sort the provided array using the bubble sort technique.
     protected static void bubbleSorting(int[] arr) {
 
         // Getting the array length.
@@ -45,5 +47,30 @@ public class BubbleSort {
 
         // Print the sorted array with the bubble sort technique.
         System.out.println("\n\nSorted array: " + Arrays.toString(arr));
+    }
+
+    // Take the user input with the min max array length validation.
+    protected static int[] takeInputAndReturnArray(int min, int max) {
+        System.out.print("Enter the number for the length of the array: ");
+        Scanner input = new Scanner(System.in);
+        int arrayLength = input.nextInt();
+
+        // Take the input from the user again if he enters the invalid input.
+        while (arrayLength < min || max < arrayLength) {
+            System.out.print("Please the valid input type between the " + min + " and " + max + ": ");
+            arrayLength = input.nextInt();
+        }
+
+        // Initialize the array as per the user's input length.
+        int[] arr = new int[arrayLength];
+
+        // Store the values into the array
+        for (int index = 0; index < arrayLength; index++) {
+            System.out.print("Please input the value for the " + index + " element: ");
+            arr[index] = input.nextInt();
+        }
+
+        // Return the user inputted array.
+        return arr;
     }
 }
